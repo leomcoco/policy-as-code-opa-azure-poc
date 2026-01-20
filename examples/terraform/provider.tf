4fbf8e4540b79cb6dac9cba73c77771a14123109
Leonardo Coco <leomcoco@gmail.com>
2026-01-20 00:53:54 -0300

    fix: remove skip_provider_registration and CI env conflict

--- examples/terraform/provider.tf
+++ examples/terraform/provider.tf
@@ -1,7 +1,5 @@
 provider "azurerm" {
   features {}
 
-  # substitui skip_provider_registration (deprecated)
-  # "none" = não tentar registrar Resource Providers automaticamente
   resource_provider_registrations = "none"
 }
